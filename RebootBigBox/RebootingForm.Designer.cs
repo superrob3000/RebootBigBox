19762e4f92124944a047525a7e162a9d79b555c7
superrob3000 <robbracero@gmail.com>
2022-02-03 18:31:25 -0500

    Add project files.

---
+++ RebootBigBox/RebootingForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace RebootBigBox
+{
+    partial class RebootingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RebootingForm));
+            this.panelWait = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelWait = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelWait.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelWait
+            // 
+            this.panelWait.BackColor = System.Drawing.Color.Black;
+            this.panelWait.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelWait.Controls.Add(this.label1);
+            this.panelWait.Controls.Add(this.labelWait);
+            this.panelWait.Controls.Add(this.pictureBox1);
+            this.panelWait.ForeColor = System.Drawing.Color.White;
+            this.panelWait.Location = new System.Drawing.Point(639, 480);
+            this.panelWait.Name = "panelWait";
+            this.panelWait.Size = new System.Drawing.Size(600, 200);
+            this.panelWait.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(592, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "(Rebooting BigBox for new settings to take effect)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelWait
+            // 
+            this.labelWait.AutoSize = true;
+            this.labelWait.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWait.ForeColor = System.Drawing.Color.White;
+            this.labelWait.Location = new System.Drawing.Point(210, 54);
+            this.labelWait.Name = "labelWait";
+            this.labelWait.Size = new System.Drawing.Size(214, 37);
+            this.labelWait.TabIndex = 0;
+            this.labelWait.Text = "Please Wait...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RebootBigBox.Properties.Resources.Omega;
+            this.pictureBox1.Location = new System.Drawing.Point(246, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RebootingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ControlBox = false;
+            this.Controls.Add(this.panelWait);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1920, 1080);
+            this.Name = "RebootingForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rebooting BigBox";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelWait.ResumeLayout(false);
+            this.panelWait.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panelWait;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelWait;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RebootBigBox/RebootingForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RebootBigBox
+{
+    public partial class RebootingForm : Form
+    {
+        public RebootingForm()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(() => { this.Reboot(); });
+        }
+
+        private void Reboot()
+        {
+            //Kill BigBox process
+            {
+                Process[] Processes = System.Diagnostics.Process.GetProcesses();
+                for (int i = 0; i < Processes.Length; i++)
+                {
+                    if (Processes[i].ProcessName.StartsWith("LaunchBox") ||
+                        Processes[i].ProcessName.StartsWith("BigBox"))
+                    {
+                        Processes[i].Kill();
+                    }
+                }
+            }
+
+            //Check if BigBox is still running
+            bool BigBoxRunning;
+            do
+            {
+                Thread.Sleep(1000);
+
+                BigBoxRunning = false;
+                Process[] Processes = System.Diagnostics.Process.GetProcesses();
+
+                for (int i = 0; i < Processes.Length; i++)
+                {
+                    if (Processes[i].ProcessName.StartsWith("LaunchBox") ||
+                        Processes[i].ProcessName.StartsWith("BigBox"))
+                    {
+                        BigBoxRunning = true;
+                    }
+                }
+
+            } while (BigBoxRunning);
+
+            //Restart BigBox
+            Process ps_bigbox = null;
+            ps_bigbox = new Process();
+            ps_bigbox.StartInfo.UseShellExecute = false;
+            ps_bigbox.StartInfo.RedirectStandardInput = false;
+            ps_bigbox.StartInfo.RedirectStandardOutput = false;
+            ps_bigbox.StartInfo.CreateNoWindow = true;
+            ps_bigbox.StartInfo.UserName = null;
+            ps_bigbox.StartInfo.Password = null;
+            ps_bigbox.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            ps_bigbox.StartInfo.FileName = Path.GetDirectoryName(Application.ExecutablePath).ToString() + "/BigBoxWithStartupMarquee.exe";
+
+            if(!File.Exists(ps_bigbox.StartInfo.FileName))
+                ps_bigbox.StartInfo.FileName = Path.GetDirectoryName(Application.ExecutablePath).ToString() + "/BigBox.exe";
+
+            ps_bigbox.Start();
+
+
+            //Exit
+            Thread.Sleep(4000);
+            this.Close();
+        }
+    }
+}
